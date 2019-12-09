c2bb8fb3afb8ee3a61ce5315a79e976e403dceaa
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2019-12-09 10:27:46 -0800

    added test to all 3 test suites

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,52 @@
 // Test away!
+// Test away
+import React from "react";
+import * as rtl from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import Controls from './Controls.js'
+import { fireEvent, render, cleanup } from "@testing-library/react";
+afterEach(cleanup);
+// test('gate can be toggled open on click.', async () => {
+//     // const toggleToOpen = jest.fn();
+//     const wrapper = rtl.render(<Controls locked={false} closed={false} />);
+//     // await wrapper.findByTestId(/open/i);
+    
+//     // const openButton = wrapper.findByTestId(/open/i);
+//     const locked = wrapper.getByText(/locked/i)
+//     expect(locked).toHaveClass('control panel');
+//     // expect(openButton).toEqual('open');
+//     // rtl.fireEvent.click(openButton);
+//     // expect(toggleToOpen).toHaveBeenCalled();
+
+// })
+
+test('looking to find button open.', async () => {
+    const toggle = jest.fn();
+    const { getByText } = render(<Controls closed={false} toggle={toggle} />);
+    const close = getByText(/close gate/i);
+    fireEvent.click(close);
+    expect(toggle).not.toHaveBeenCalled();
+    
+})
+
+test('looking to find button.', async () => {
+    const toggle = jest.fn();
+    const { getByText } = render(<Controls locked={true} toggle={toggle} />);
+    const locked = getByText(/lock gate/i);
+    fireEvent.click(locked);
+    expect(toggle).not.toHaveBeenCalled();
+    
+})
+//cleanup for testing
+
+test('if close toggle is disabled when locked.', async () => {
+    const toggle = jest.fn()
+    const { getByTestId } = render(<Controls locked={true} toggle={toggle} />);
+    const closed = getByTestId('closed');
+    const locked = getByTestId('locked');
+    fireEvent.click(closed);
+    fireEvent.click(locked);
+    expect(closed).toBeDisabled()
+})
+
+
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
-// Test away
+import React from "react";
+import * as rtl from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+import { fireEvent } from "@testing-library/react";
+
+test('<Dashboard> SnapShot', async () => {
+    const wrapper = rtl.render(<Dashboard />);
+
+    expect(wrapper.asFragment()).toMatchSnapshot();
+
+})
+
